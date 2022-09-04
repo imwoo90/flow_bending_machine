14287f0ff534e60a1b9bd0c3b0b0cc663bbb8a4a
imwoo90 <wim90@kakao.com>
2022-09-04 10:47:03 +0900

    first commit

---
+++ src/device/Keypad_4x3/Keypad_4x3.cpp
@@ -0,0 +1,80 @@
+#include <FreeRTOS.h>
+#include <task.h>
+
+#include "Keypad_4x3.h"
+#include <vector>
+
+static std::vector<std::function<void(const char)> > _callbacks;
+
+void Keypad_4x3::pin_mode(byte pinNum, byte mode) {
+    // pcf8574.pinMode(pinNum, mode);
+}
+void Keypad_4x3::pin_write(byte pinNum, boolean level) {
+    pcf8574.digitalWrite(pinNum, level);
+}
+int  Keypad_4x3::pin_read(byte pinNum) {
+    int read = pcf8574.digitalRead(pinNum, true);
+    return read;
+}
+
+static void notify() {
+    Keypad_4x3* kp = Keypad_4x3::getInstance();
+    char key = kp->getKey();
+    if (key == NO_KEY)
+        return;
+
+    for (auto i = 0; i < _callbacks.size(); i++) {
+        _callbacks[i](key);
+    }
+}
+
+static void scan_task(void *params) {
+    while(1) {
+        notify();
+        delay(50);
+    }
+}
+
+static void task_initialize() {
+    enum {
+        Priority = configMAX_PRIORITIES / 2 - 1,
+    };
+
+    static TaskHandle_t _keypadTask;
+    xTaskCreate(scan_task, "KEYPAD_4X3", 1024, 0, Priority, &_keypadTask);
+}
+
+Keypad_4x3::Keypad_4x3(char *userKeymap, byte *row, byte *col, byte numRows, byte numCols) :
+    Keypad(userKeymap, row, col, numRows, numCols), pcf8574(0x20) {
+    for(int r = 0; r < numRows; r++)
+        pcf8574.pinMode(row[r], INPUT_PULLUP);
+    for(int c = 0 ; c < numCols; c++) {
+        pcf8574.pinMode(col[c], OUTPUT);
+        pcf8574.digitalWrite(col[c], HIGH);
+    }
+    pcf8574.begin();
+    task_initialize();
+}
+
+Keypad_4x3* Keypad_4x3::getInstance() {
+    static const byte _ROWS = 4; //four rows
+    static const byte _COLS = 3; //three columns
+    static byte _rowPins[_ROWS] = {P0, P1, P2, P3}; //connect to the row pinouts of the keypad
+    static byte _colPins[_COLS] = {P6, P5, P4}; //connect to the column pinouts of the keypad
+    static char _keys[_ROWS][_COLS] = {
+        {'1','2','3'},
+        {'4','5','6'},
+        {'7','8','9'},
+        {'*','0','#'}
+    };
+    static Keypad_4x3* singleton_instance = NULL;
+
+    if(singleton_instance == NULL) {
+        singleton_instance = new Keypad_4x3(makeKeymap(_keys), _rowPins, _colPins, _ROWS, _COLS);
+    }
+    return singleton_instance;
+}
+
+void Keypad_4x3::subscribe(std::function<void(const char)> callback) {
+    _callbacks.push_back(callback);
+}
---
+++ src/device/Keypad_4x3/Keypad_4x3.h
@@ -0,0 +1,22 @@
+#ifndef KEYPAD_4X3_H
+#define KEYPAD_4X3_H
+
+#include <functional>
+#include <Keypad.h>
+#include <PCF8574.h>
+
+class Keypad_4x3 : public Keypad {
+private:
+    PCF8574 pcf8574;
+    Keypad_4x3(char *userKeymap, byte *row, byte *col, byte numRows, byte numCols);
+public:
+    virtual void pin_mode(byte pinNum, byte mode);
+    virtual void pin_write(byte pinNum, boolean level);
+    virtual int  pin_read(byte pinNum);
+
+    static Keypad_4x3* getInstance();
+    void subscribe(std::function<void(const char)> callback);
+};
+
+
+#endif
